211454b4ef4d72f40443593ac9fa4f707f49c26e
Fabiano Hino <fabianohino@gmail.com>
2021-04-13 22:22:57 -0300

    Change DSL repository to fabianohino

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,8 +1,8 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('git://github.com/FabianoHino/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('FH DSL user')
+            node / gitConfigEmail('fabianohino@gmail.com')
         }
     }
     triggers {
